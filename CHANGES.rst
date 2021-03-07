4bc65b4892c22f80e6e8cc3553a1f8ad0e906559
Niyamaka <niyamaka@yuneta.io>
2021-03-07 09:23:37 +0100

    v4.10.9

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,4 +1,4 @@
-Version "4.10.8" "a"
+Version "4.10.9" "a"
 ===================
 
 Simplify files of original project.
--- javascript/mxClient.js
+++ javascript/mxClient.js
@@ -22,7 +22,7 @@ var mxClient =
      * versionMajor.versionMinor.buildNumber.revisionNumber
      *
      */
-    VERSION: "4.10.8",
+    VERSION: "4.10.9",
 
     /**
      * Variable: IS_IE
